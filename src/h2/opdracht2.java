813b59ac9cbfe98afe47939d6cc339cb8c449237
robincreebsburg <Waterrat123>
2019-02-13 14:40:01 +0100

    h

---
+++ src/h2/opdracht2.java
@@ -0,0 +1,16 @@
+package h2;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class opdracht2 extends Applet {
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Robin",50, 60);
+    }
+}
