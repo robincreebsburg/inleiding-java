fc48e0bfb1281749e6ba33abe23806d0791a742f
robincreebsburg <Waterrat123>
2019-02-15 09:52:48 +0100

    h

--- src/h4/opdracht47.java
+++ src/h4/opdracht47.java
@@ -1,13 +1,18 @@
 package h4;
 
 import java.applet.Applet;
+import java.awt.*;
 
 public class opdracht47 extends Applet {
     public void init() {
 
     }
 
-    public void paint() {
-
+    public void paint(Graphics g) {
+        g.drawRoundRect(140, 120, 100, 100, 10, 10);
+        g.fillArc(160, 140, 15, 15, 720, 360);
+        g.fillArc(205, 140, 15, 15, 720, 360);
+        g.fillArc(160, 185, 15, 15, 720, 360);
+        g.fillArc(205, 185, 15, 15, 720, 360);
     }
 }
